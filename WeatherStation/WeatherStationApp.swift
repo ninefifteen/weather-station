afe6515b0877021a103096f6a69bcc29b3138eee
Shawn Seals <shawn.seals915@yahoo.com>
2023-01-14 12:30:35 -0600

    Initial Xcode project.

---
+++ WeatherStation/WeatherStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherStationApp.swift
+//  WeatherStation
+//
+//  Created by Shawn Seals on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
