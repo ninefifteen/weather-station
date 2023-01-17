5ff1bfa53d50a79e067cdbbdbb1fd218947beec3
Shawn Seals <shawn.seals915@yahoo.com>
2023-01-17 00:24:16 -0600

    Safe area compensation for point visibility calculation.

--- WeatherStation/Utilities/Extensions/CLLocationCoordinate2D+Extensions.swift
+++ WeatherStation/Utilities/Extensions/CLLocationCoordinate2D+Extensions.swift
@@ -11,6 +11,15 @@ import MapKit
 extension CLLocationCoordinate2D {
     func isContained(in mapRect: MKMapRect) -> Bool {
         let mapPoint = MKMapPoint(self)
-        return mapRect.contains(mapPoint)
+        // MKMapView.visibleMapRect(_:animated:) does not include safe area. We add
+        // add a buffer to the rect so that annotations are displayed until the point
+        // is out of the user's view.
+        let safeAreaCompensatingMapRect = MKMapRect(
+            x: mapRect.origin.x - 0.1 * mapRect.size.width,
+            y: mapRect.origin.y - 0.05 * mapRect.size.height,
+            width: mapRect.size.width * 1.2,
+            height: mapRect.size.height * 1.2
+        )
+        return safeAreaCompensatingMapRect.contains(mapPoint)
     }
 }
