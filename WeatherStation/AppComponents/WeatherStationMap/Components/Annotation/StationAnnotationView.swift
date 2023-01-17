127660660db553f694ccb281a5346b05eb09a9bd
Shawn Seals <shawn.seals915@yahoo.com>
2023-01-16 20:58:40 -0600

    StationAnnotationView changes when selected.

--- WeatherStation/AppComponents/WeatherStationMap/Components/Annotation/StationAnnotationView.swift
+++ WeatherStation/AppComponents/WeatherStationMap/Components/Annotation/StationAnnotationView.swift
@@ -36,30 +36,53 @@ class StationAnnotationView: MKAnnotationView {
     
     // MARK: - Properties
     
-    private lazy var containerView: UIView = {
-        let view = UIView(frame: .zero)
-        view.backgroundColor = .red
-        view.layer.cornerRadius = 16.0
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
     private lazy var bottomCornerView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .red
-        view.layer.cornerRadius = 4.0
+        view.layer.cornerRadius = 4
         return view
     }()
     
+    private lazy var bubbleView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .red
+        view.layer.cornerRadius = 16
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var normalView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .clear
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var selectedView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .red
+        view.layer.cornerRadius = 5
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.opacity = 0
+        return view
+    }()
+    
+    let normalAnchorPoint = CGPointMake(0.5, 0.87)
+    let selectedAnchorPoint = CGPointMake(0.5, 0.14)
+    
+    // MARK: - Functions
+    
     private func configureView() {
         frame = CGRect(x: 0, y: 0, width: 50, height: 50)
-        layer.anchorPoint = CGPointMake(0.5, 1.0)
-        backgroundColor = .blue
+        layer.anchorPoint = normalAnchorPoint
+        backgroundColor = .clear
         
-        addSubview(bottomCornerView)
-        addSubview(containerView)
-        containerView.addSubview(label)
+        addSubview(normalView)
+        normalView.addSubview(bottomCornerView)
+        normalView.addSubview(bubbleView)
+        bubbleView.addSubview(label)
+        addSubview(selectedView)
         
         let angle = (45.0 * CGFloat.pi) / 180
         let transform = CGAffineTransform(rotationAngle: angle)
@@ -68,26 +91,48 @@ class StationAnnotationView: MKAnnotationView {
     
     private func configureConstraints() {
         NSLayoutConstraint.activate([
-            bottomCornerView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
-            bottomCornerView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            bottomCornerView.topAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -12),
-            bottomCornerView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            normalView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            normalView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            normalView.topAnchor.constraint(equalTo: bubbleView.topAnchor),
+            normalView.leadingAnchor.constraint(equalTo: bubbleView.leadingAnchor),
+            normalView.trailingAnchor.constraint(equalTo: bubbleView.trailingAnchor),
+        ])
+        
+        NSLayoutConstraint.activate([
+            bottomCornerView.bottomAnchor.constraint(equalTo: normalView.bottomAnchor, constant: -8),
+            bottomCornerView.centerXAnchor.constraint(equalTo: normalView.centerXAnchor),
+            bottomCornerView.topAnchor.constraint(equalTo: bubbleView.bottomAnchor, constant: -12),
+            bottomCornerView.centerXAnchor.constraint(equalTo: bubbleView.centerXAnchor),
             bottomCornerView.widthAnchor.constraint(equalToConstant: 18),
             bottomCornerView.heightAnchor.constraint(equalToConstant: 18),
         ])
         
         NSLayoutConstraint.activate([
-            containerView.widthAnchor.constraint(greaterThanOrEqualToConstant: 50),
-            containerView.heightAnchor.constraint(greaterThanOrEqualToConstant: 30),
+            bubbleView.widthAnchor.constraint(greaterThanOrEqualToConstant: 50),
+            bubbleView.heightAnchor.constraint(greaterThanOrEqualToConstant: 30),
         ])
         
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
-            label.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 8),
-            label.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -8),
-            label.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
-            label.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10),
+            label.centerXAnchor.constraint(equalTo: bubbleView.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: bubbleView.centerYAnchor),
+            label.topAnchor.constraint(equalTo: bubbleView.topAnchor, constant: 8),
+            label.bottomAnchor.constraint(equalTo: bubbleView.bottomAnchor, constant: -8),
+            label.leadingAnchor.constraint(equalTo: bubbleView.leadingAnchor, constant: 10),
+            label.trailingAnchor.constraint(equalTo: bubbleView.trailingAnchor, constant: -10),
         ])
+        
+        NSLayoutConstraint.activate([
+            selectedView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            selectedView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            selectedView.heightAnchor.constraint(equalToConstant: 10),
+            selectedView.widthAnchor.constraint(equalToConstant: 10),
+        ])
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        layer.anchorPoint = selected ? selectedAnchorPoint : normalAnchorPoint
+        normalView.layer.opacity = selected ? 0 : 1
+        selectedView.layer.opacity = selected ? 1 : 0
     }
 }
